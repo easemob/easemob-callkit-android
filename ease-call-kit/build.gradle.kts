a24f07320197cc097da1fdebb82c66ebed397c21
刘吉南 <neil.liu@easemob.com>
2026-02-27 13:57:22 +0800

    Update SDK version to 4.19.1

--- ease-call-kit/build.gradle.kts
+++ ease-call-kit/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     // 集成 lite SDK
     api("cn.shengwang.rtc:lite-sdk:4.6.0")
 
-    api("io.hyphenate:hyphenate-chat:4.19.0")
+    api("io.hyphenate:hyphenate-chat:4.19.1")
 //    implementation(project(mapOf("path" to ":hyphenatechatsdk")))
 
 }
