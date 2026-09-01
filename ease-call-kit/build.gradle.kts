305f4d0bef2b3169aa091d2a18463ec945372115
xuchengpu mac <1550540124@qq.com>
2026-09-01 13:50:46 +0800

    update sdk version to 4.16.4

--- ease-call-kit/build.gradle.kts
+++ ease-call-kit/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     // 集成 lite SDK
     api("cn.shengwang.rtc:lite-sdk:4.6.0")
 
-    api("io.hyphenate:hyphenate-chat:4.24.0")
+    api("io.hyphenate:hyphenate-chat:4.16.4")
 //    implementation(project(mapOf("path" to ":hyphenatechatsdk")))
 
 }
